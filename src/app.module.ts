9f3d89481bfffa826a31b154a6f387b0b736b129
godbs129 <junhaeyoon@gmail.com>
2022-06-29 10:12:54 +0900

    ADD: database module 추가 및 서버 포트 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './config/database/database.module';
+import { PointModule } from './point/point.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    PointModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
