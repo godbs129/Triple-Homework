0eddde090e0324cf0487d65c45c67fea88c21539
godbs129 <junhaeyoon@gmail.com>
2022-06-29 16:15:50 +0900

    ADD: Exception Filter 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import CatchException from './common/filter/ExceptionFilter';
 import { DatabaseModule } from './config/database/database.module';
 import { PointModule } from './event/event.module';
 
@@ -14,6 +16,12 @@ import { PointModule } from './event/event.module';
     PointModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: CatchException,
+    },
+    AppService,
+  ],
 })
 export class AppModule {}
