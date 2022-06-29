d4811cd2625219e5adcd84d926be5c8f155b22c8
godbs129 <junhaeyoon@gmail.com>
2022-06-29 16:16:22 +0900

    ADD: 리뷰 작성 후 포인트 부여 기능 추가

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Response } from 'src/common/response/Response';
+import { EventDto } from './dto/event.dto';
 import { EventService } from './event.service';
 
 @Controller('event')
 export class EventController {
   constructor(private readonly eventService: EventService) {}
 
-  // @Post('/')
-  // async;
+  @Post('/')
+  async addPoint(@Body() eventDto: EventDto) {
+    await this.eventService.checkActionType(eventDto);
+
+    return Response.ok('포인트 부여 성공');
+  }
 }
--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ActionType } from 'src/common/enum/point.enum';
 import { Repository } from 'typeorm';
+import { EventDto } from './dto/event.dto';
 import { Point } from './entity/Point';
 
 @Injectable()
@@ -9,4 +11,51 @@ export class EventService {
     @InjectRepository(Point)
     private readonly pointRepository: Repository<Point>,
   ) {}
+
+  async checkActionType(eventDto: EventDto) {
+    const { action } = eventDto;
+
+    if (action === ActionType.ADD) {
+      await this.addReviewPoint(eventDto);
+    } else if (action === ActionType.MOD) {
+      this.modReviewPoint(eventDto);
+    } else {
+    }
+  }
+
+  async addReviewPoint(eventDto: EventDto): Promise<void> {
+    let point: number = 0;
+
+    const isDuplicatedReview: Point = await this.getPointByUserAndReview(
+      eventDto.userId,
+      eventDto.reviewId,
+    );
+    if (isDuplicatedReview !== null) {
+      throw new ConflictException('이미 작성한 리뷰입니다');
+    }
+
+    if (eventDto.content.length >= 1) point++;
+    if (eventDto.attachedPhotoIds.length >= 1) point++;
+
+    const addedPoint = this.pointRepository.create({
+      reviewId: eventDto.reviewId,
+      userId: eventDto.userId,
+      point: point,
+    });
+    await this.pointRepository.save(addedPoint);
+  }
+
+  async modReviewPoint(eventDto: EventDto): Promise<void> {}
+
+  async getPointByUserAndReview(
+    userId: string,
+    reviewId: string,
+  ): Promise<Point> {
+    return this.pointRepository.findOne({
+      where: {
+        userId: userId,
+        reviewId: reviewId,
+      },
+    });
+  }
 }
