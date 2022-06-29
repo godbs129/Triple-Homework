8ddda99f1dbe41ac09116055713a199c238c205b
godbs129 <junhaeyoon@gmail.com>
2022-06-29 13:34:01 +0900

    FIX: database config entity 추가 및 database env명 수정

--- src/config/database/TypeormConfig.service.ts
+++ src/config/database/TypeormConfig.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Point } from 'src/point/entity/Point';
 import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
 
 @Injectable()
@@ -13,9 +14,9 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       host: this.configService.get<string>('DB_HOST'),
       username: this.configService.get<string>('DB_USER'),
       password: this.configService.get<string>('DB_PASSWORD'),
-      database: this.configService.get<string>('DB_DB'),
+      database: this.configService.get<string>('DB_DATABASE'),
       port: +this.configService.get<number>('DB_PORT'),
-      entities: [],
+      entities: [Point],
       synchronize: true,
     };
   }
